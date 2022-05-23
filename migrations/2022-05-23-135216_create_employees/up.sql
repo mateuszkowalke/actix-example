52b61decf11f00ab9d56746c69733f0d25bf72e8
mateusz.kowalke <mateusz.kowalke@aurafutures.com>
2022-05-23 16:07:30 +0200

    initial commit

---
+++ migrations/2022-05-23-135216_create_employees/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    department VARCHAR,
+    salary INT,
+    age INT
+);
---
+++ src/schema.rs
@@ -0,0 +1,10 @@
+table! {
+    employees (id) {
+        id -> Int4,
+        first_name -> Nullable<Varchar>,
+        last_name -> Nullable<Varchar>,
+        department -> Nullable<Varchar>,
+        salary -> Nullable<Int4>,
+        age -> Nullable<Int4>,
+    }
+}
